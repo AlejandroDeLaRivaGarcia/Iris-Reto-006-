cbd61ba0206ccb54769f2dcd177f97faa04f8433
midas@tower <javier.liano@alumnos.uneatlantico.es>
2025-10-28 22:41:38 +0100

    fix: correct relative imports in microservices main.py files and add user router placeholder

--- src/servicio-asignaturas/main.py
+++ src/servicio-asignaturas/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from . import models
-from .session import engine
-from .router import router
+from app import models
+from app.session import engine
+from app.router import router
 
 models.Base.metadata.create_all(bind=engine)
 
