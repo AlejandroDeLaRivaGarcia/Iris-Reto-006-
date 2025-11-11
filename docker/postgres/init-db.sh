06737ed27e96d442164d0c8a60aed031436882da
midas@tower <javier.liano@alumnos.uneatlantico.es>
2025-11-11 17:32:34 +0100

    feat: Separar datos iniciales de la configuración de la base de datos
    Se han movido las sentencias INSERT de docker/postgres/init-db.sh a un nuevo archivo docker/postgres/seed.sql.
    Esto permite una mejor separación de la configuración de la base de datos y los datos iniciales.
    Además, se ha actualizado docker-compose.yml para montar el nuevo archivo seed.sql y .gitignore para ignorarlo.

--- docker/postgres/init-db.sh
+++ docker/postgres/init-db.sh
@@ -45,25 +45,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "iris" <<-EOSQL
         FOREIGN KEY (teacher_id) REFERENCES teachers (id) ON DELETE CASCADE,
         FOREIGN KEY (subject_code) REFERENCES subjects (code) ON DELETE CASCADE
     );
-
-    INSERT INTO subjects (code, name) VALUES
-    ('IYA023', 'Bases de Datos I'),
-    ('CSJ038', 'Estadística I'),
-    ('IYA025', 'Estructuras de Datos y Algoritmos I'),
-    ('IYA028', 'Matemática Numérica'),
-    ('IYA022', 'Redes de Ordenadores');
-
-    INSERT INTO teachers (id, name) VALUES
-    (1, 'Lorenzo Perez Brito'),
-    (2, 'Jose Manuel Breñosa Martínez'),
-    (3, 'Manuel Antonio Masías Vergara'),
-    (4, 'Jorge Crespo Álvarez'),
-    (5, 'Mariano Benito Hoz');
-
-    INSERT INTO teacher_subjects (teacher_id, subject_code) VALUES
-    (1, 'IYA023'),
-    (2, 'CSJ038'),
-    (3, 'IYA025'),
-    (4, 'IYA028'),
-    (5, 'IYA022');
 EOSQL
